2184a27cc6c69f0c08145f5f0d3bc7d027d232bc
JalenL616 <jalenlocke@gmail.com>
2024-10-29 14:43:17 -0700

    Start button?

---
+++ Bee project/Assets/ButtonScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
--- Bee project/Assets/Scripts/Camera_Controller.cs
+++ Bee project/Assets/Scripts/Camera_Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Camera_Controller : MonoBehaviour
 {
@@ -52,6 +53,13 @@ public class Camera_Controller : MonoBehaviour
         {
             newPosition += (transform.right * -movementSpeed);
         }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        
 
         // //if(Input.GetKey(KeyCode.Z))
         // {
