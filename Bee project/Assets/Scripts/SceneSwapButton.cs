12392139e118cb405b4eedef18605068ecdc744b
JalenL616 <jalenlocke@gmail.com>
2024-11-01 14:41:47 -0700

    Revert "Revert "Demo update (test)""
    This reverts commit de0ce0fcae0c851321680e768944734790cce6be.

---
+++ Bee project/Assets/Scripts/SceneSwapButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapButton : MonoBehaviour
+{
+    [SerializeField] private string newSceneName;
+    // Start is called before the first frame update
+        void Start()
+    {
+
+    }
+        void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        ChangeScene();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(newSceneName);
+
+    }
+}
