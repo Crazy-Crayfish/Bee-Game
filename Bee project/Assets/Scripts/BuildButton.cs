57dd351ac9bc7701bdbd1d982c325a646a2c6ce1
JalenL616 <jalenlocke@gmail.com>
2024-12-11 23:53:03 -0800

    Build Menu

---
+++ Bee project/Assets/Scripts/BuildButton.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildButton : MonoBehaviour
+{
+    private bool popUpOpen;
+    [SerializeField] GameObject popUpMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUpOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void popUp()
+    {
+        if (popUpOpen)
+        {
+            popUpMenu.transform.position = new Vector3(-1000, 0, 0);
+            popUpOpen = false;
+        }
+        else
+        {
+            popUpMenu.transform.position = new Vector3(95, 270, 0);
+            popUpOpen = true;
+        }
+    }
+}
