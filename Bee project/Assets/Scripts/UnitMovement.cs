aaf42381da24062a7d4a4e2c421f5f06eaf6febb
David Hodack <dhodack@gmail.com>
2024-12-12 12:04:07 -0800

    Full integration, demo ready
    Still needs some art updates, maybe camera aspect ratio change too.

--- Bee project/Assets/Scripts/UnitMovement.cs
+++ Bee project/Assets/Scripts/UnitMovement.cs
@@ -23,18 +23,21 @@ public class UnitMovement : MonoBehaviour {
     
     void SetDestinationToMousePosition() {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        // if (Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, ground)) 
-        // {
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
-        // RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
-        agent.destination = hit.collider.gameObject.transform.position;  
-        // }
-        // var target = cam.ScreenToWorldPoint(Input.mousePosition);
-        // target.z = 0;
-        // agent.destination = target;
+        // Debug.Log("function triggered");
 
-        // Vector2 worldPoint = Camera.main.ScreenToWorldPoint( Input.mousePosition );
-        // RaycastHit2D hit = Physics2D.Raycast( worldPoint, Vector2.zero );
-        // agent.SetDestination(hit.point);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+ 
+        // RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        
+        agent.destination = hit.collider.gameObject.transform.position;  
+        
+
+        // if (Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity))
+        // {
+        //     Debug.Log(gameObject.transform.position + " /// " + agent.destination);
+        // }
+        // }
+
     }
+
 }
