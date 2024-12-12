f34c5f292e2282d6187094642af2a2203753258c
JalenL616 <jalenlocke@gmail.com>
2024-12-11 23:53:03 -0800

    Build Menu

--- Bee project/Assets/Scripts/SceneButton.cs
+++ Bee project/Assets/Scripts/SceneButton.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ButtonScript : MonoBehaviour
+public class SceneButton : MonoBehaviour
 {
 
     // Start is called before the first frame update
