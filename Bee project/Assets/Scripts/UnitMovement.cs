f6dc3e16e26974edce7041fd1c8c6570dad5bd40
David Hodack <dhodack@gmail.com>
2024-12-11 23:39:12 -0800

    Adding bee movement part of the demo version
    Major new additions. NavMeshPlus API may not work on others' devices

---
+++ Bee project/Assets/Scripts/UnitMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMovement : MonoBehaviour {
+    Camera cam;
+    NavMeshAgent agent;
+    void Start() {
+        cam = Camera.main;
+		agent = GetComponent<NavMeshAgent>();
+		agent.updateRotation = false;
+		agent.updateUpAxis = false;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        // Is RMB down?
+        if (Input.GetMouseButtonDown(1)) {
+            SetDestinationToMousePosition();
+        }
+    }
+    
+    void SetDestinationToMousePosition() {
+        RaycastHit2D hit = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        agent.destination = hit.collider.gameObject.transform.position;  
+        // var target = cam.ScreenToWorldPoint(Input.mousePosition);
+        // target.z = 0;
+        // agent.destination = target;
+
+        // Vector2 worldPoint = Camera.main.ScreenToWorldPoint( Input.mousePosition );
+        // RaycastHit2D hit = Physics2D.Raycast( worldPoint, Vector2.zero );
+        // agent.SetDestination(hit.point);
+    }
+}
