9ba31108042f8c6737f95ce9f7e7085390686102
David Hodack <dhodack@gmail.com>
2025-01-16 14:56:51 -0800

    Purging of evil assets folder

--- Bee project/Assets/Scripts/SceneSwapButton.cs
+++ Bee project/Assets/Scripts/SceneSwapButton.cs
@@ -1,18 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class SceneSwapButton : MonoBehaviour
 {
     [SerializeField] private string newSceneName;
+    [SerializeField] private string mode;
+    
     // Start is called before the first frame update
         void Start()
     {
 
     }
-        void Update()
-    {
 
-    }
     void OnMouseDown()
     {
         Debug.Log("change scene");
@@ -21,8 +21,26 @@ public class SceneSwapButton : MonoBehaviour
 
     public void ChangeScene()
     {
-        
-        SceneManager.LoadScene(newSceneName);
+        // if (mode == "Additive")
+        // {
+        //     GameObject[] obj = Scene.GetRootGameObjects();
+        //     foreach (object o in obj)
+        //     {
+        //         if (o.GetComponent<SpriteRenderer> () != null)
+        //         {
+        //             o.GetComponent<SpriteRenderer>.SetActive(false);
+        //             // Debug.Log(o.name);
+        //         }
+                
+                
+        //     }
+        //     SceneManager.LoadScene(newSceneName, LoadSceneMode.Additive);
+        // } 
+        // else
+        // {
+        //     SceneManager.LoadScene(newSceneName, LoadSceneMode.Single);
+        // }
+        SceneManager.LoadScene(newSceneName, LoadSceneMode.Single);
 
     }
 }
