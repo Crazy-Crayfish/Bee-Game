328f45b5183ac501ca050f69603381f966239341
IshaanVarma <IshaanVarma1@gmail.com>
2025-01-17 15:00:07 -0800

    trees
    trees that look like the flower embed

--- Bee project/Assets/Scripts/GridManager.cs
+++ Bee project/Assets/Scripts/GridManager.cs
@@ -16,6 +16,7 @@ public class GridManager : MonoBehaviour
     [SerializeField] private Tile blueFlower;
     [SerializeField] private Tile sunFlower;
     [SerializeField] private Tile redFlower;
+    [SerializeField] private Tile treeTile;
     // Not a real difficultyModifier Yet
 
     void Start() {
@@ -36,7 +37,7 @@ public class GridManager : MonoBehaviour
     }
    
    camera.transform.position = new Vector3((float)width/2 -0.5f, (float)height / 2 -0.5f,-20);
-   hive.transform.parent.transform.position = new Vector3((float)width/2 -0.5f, (float)height / 2 -0.5f,-10);
+   hive.transform.position = new Vector3((float)width/2 -0.5f, (float)height / 2 -0.5f,-10);
     Random rnd = new Random();
     int seed = rnd.Next(1111111,9999999);
 
@@ -44,13 +45,10 @@ public class GridManager : MonoBehaviour
 
    }
    void GenerateElements(int modifier){
-    // int flowerMod = modifier/100000;
-    // can be used to make game easier
-    // if(flowerMod > 43 ) {flowerMod /= 2; }
     long flowerMod = (long) Math.Pow(modifier, difficultyModifier);
  
     GenerateFlowers(flowerMod);
-
+    GenerateTrees();
 
 
    }
@@ -86,22 +84,22 @@ public class GridManager : MonoBehaviour
 
         if(zzz>=0 && zzz <= 39) {
             var spawnedTile = Instantiate(pinkFlower, new Vector3(z,y+0.1f), Quaternion.identity);
-            spawnedTile.name = $"pinkFlowerTile {z} {y}";
+            spawnedTile.name = $"FlowerTile {z} {y}";
             spawnedTile.setVal(50);
         }
         else if (zzz >= 40 && zzz <= 69) {
             var spawnedTile = Instantiate(blueFlower, new Vector3(z,y+0.1f), Quaternion.identity);
-            spawnedTile.name = $"blueFlowerTile {z} {y}";
+            spawnedTile.name = $"FlowerTile {z} {y}";
             spawnedTile.setVal(80);
         }
         else if (zzz >= 70 && zzz <= 89) {
             var spawnedTile = Instantiate(sunFlower, new Vector3(z,y+0.1f), Quaternion.identity);
-            spawnedTile.name = $"sunFlowerTile {z} {y}";
+            spawnedTile.name = $"FlowerTile {z} {y}";
             spawnedTile.setVal(100);
         }
          else {
             var spawnedTile = Instantiate(redFlower, new Vector3(z,y+0.1f), Quaternion.identity);
-            spawnedTile.name = $"redFlowerTile {z} {y}";
+            spawnedTile.name = $"FlowerTile {z} {y}";
             spawnedTile.setVal(150);
         }
 
@@ -113,70 +111,46 @@ public class GridManager : MonoBehaviour
     
     }
    }
+
+
+
+
+   
+   
+   void GenerateTrees(){ int area = height * width; 
+   int y = 0; 
+   Random rnd = new Random(); 
+//    long treeSeed = (long)Math.Pow(rnd.Next(1111111, 9999999), difficultyModifier); 
+//    long treetemp = treeSeed; 
+   for (int x = 0; x < area; x += (int)(rnd.Next(1,100))) { 
+    int z = x % width; 
+    y = x / width; 
+    // long zzz = treetemp % 100; 
+    // treetemp /= 100; 
+    // if (treetemp == 0) { 
+    //     treeSeed = treeSeed * 2; 
+    //     treetemp = treeSeed; 
+    //     } 
+     if (!IsFlowerTile(z, y)) { 
+        Debug.Log($"Attempting to instantiate treeTile at {z}, {y}");
+        var spawnedTile = Instantiate(treeTile, new Vector3(z, y), Quaternion.identity); 
+        spawnedTile.name = $"treeTile {z} {y}"; 
+            
+         }
+             } 
+   }
+   
+   
+   
+   
+    bool IsFlowerTile(int x, int y) { // Check if a tile at position (x, y) is a flower tile 
+   var tileName = $"FlowerTile {x} {y}";
+    GameObject tileObject = GameObject.Find(tileName); 
+    if (tileObject != null && (tileObject.name.Contains("Flower"))) 
+    { 
+        return true; 
+    } 
+    return false;
 }
 
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using System;
-// using Random = System.Random;
-
-// public class GridManager : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     [SerializeField] private int width, height;
-//     [SerializeField] private Tile tilePreFab;
-//     [SerializeField] private Transform camera;
-//     [SerializeField] private Transform hive;
-//        [SerializeField] private Tile flowerTilePreFab;
-
-//     void Start() {
-//         GenerateGrid();
-//     }
-
-//    void GenerateGrid(){
-//     for (int x = 0; x < width; x++) {
-//         for (int y = 0; y < height; y++) {
-            
-//             var spawnedTile = Instantiate(tilePreFab, new Vector3(x,y), Quaternion.identity);
-//             spawnedTile.name = $"Tile {x} {y}";
-//             var isOffSet = (x % 2 == 0 && y%2 !=0) || (y%2 == 0 && x%2 != 0);
-//             spawnedTile.Init(isOffSet);
-
-//         }
-//     }
-   
-//    camera.transform.position = new Vector3((float)width/2 -0.5f, (float)height / 2 -0.5f,-20);
-//    hive.transform.position = new Vector3((float)width/2 -0.5f, (float)height / 2 -0.5f,-10);
-//     Random rnd = new Random();
-//     int seed = rnd.Next(1111111,9999999);
-
-//     GenerateElements(seed);
-
-//    }
-//    void GenerateElements(int modifier){
-//     int flowerMod = modifier/100000;
-//     // can be used to make game easier
-//     if(flowerMod > 43 ) {flowerMod /= 2; }
-//     GenerateFlowers(flowerMod);
-
-
-
-//    }
-
-
-//    void GenerateFlowers(int mod){
-//     int area = height*width;
-//     int y = 0;
-//     Random rnd1 = new Random();
-    
-//     for(int x = 0; x<area; x+=rnd1.Next(1, mod)) {
-//         int z = x%width;
-//         y = x/width;
-//          var spawneTile = Instantiate(flowerTilePreFab, new Vector3(z,y), Quaternion.identity);
-//             spawneTile.name = $"flowerTile {z} {y}";
-
-
-//     }
-//    }
-// }
+}
